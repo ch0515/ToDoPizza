816afe552c9ce1ad7dedec8a32af3d139df843f7
chaemin <w2114@e-mirim.hs.kr>
2023-05-23 22:37:07 +0900

    현재 날짜 가져와서 표시

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const datetext = document.getElementById("datetext");
+function getdate() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth();
+  const date = now.getDate();
+
+  datetext.innerText = `${year} - ${month + 1} - ${date}`;
+}
+
+getdate();
