f8b67a3194a586ba32365f1d9900a756b29f90f3
chaemin <w2114@e-mirim.hs.kr>
2023-05-24 20:28:04 +0900

    캘린더 수정

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-const showToday = () => {
-    let now = new Date();
-    let year = now.getFullYear();
-    let month = now.getMonth() + 1;
-    let date = now.getDate();
+// const showToday = () => {
+//     let now = new Date();
+//     let year = now.getFullYear();
+//     let month = now.getMonth() + 1;
+//     let date = now.getDate();
 
-    let title = `${year} - ${month} - ${date}`;
+//     let title = `${year} - ${month} - ${date}`;
 
-    let cardDateDivs = document.querySelectorAll(".card-date");
-    for (cardDateDiv of cardDateDivs) {
-        cardDateDiv.innerHTML = title;
-    }
-}
-showToday();
+//     let cardDateDivs = document.querySelectorAll(".card-date");
+//     for (cardDateDiv of cardDateDivs) {
+//         cardDateDiv.innerHTML = title;
+//     }
+// }
+// showToday();
