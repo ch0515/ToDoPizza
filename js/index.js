378d1902df37f063fb22aca9eef227ad119a6487
Choi Wonseo <cws4725@gmail.com>
2023-05-30 10:49:56 +0900

    글자 자르기 및 상세설명 뜨게

--- js/index.js
+++ js/index.js
@@ -1,38 +1,52 @@
 const targetaddbtn = document.getElementById("add_btn");
 targetaddbtn.addEventListener("click", () => {
-  /* 
-        <div class="target" id="target">
-          <p class="targetText">학교</p>
-          <input type="button" class="list_add_btn" id="list_add_btn">
-        </div> */
   const target2 = document.createElement("div");
   target2.id = "target2";
   target2.className = "target2";
+  
+  const truncatedTextId = "truncatedText"; // 잘려서 보이는 p 요소에 지정할 아이디
+  
   const targetText = document.createElement("p");
-  targetText.innerHTML = "대충!~`~~~ 긴 내용";
+  targetText.innerHTML = "안녕하세요~~~";
+  // 목표 글씨 전체 보이게 하기
+  targetText.setAttribute("title", targetText.innerHTML);
   targetText.id = "targetText2";
   targetText.className = "targetText2";
   const list_add_btn = document.createElement("input");
   list_add_btn.id = "list_add_btn";
   list_add_btn.className = "list_add_btn";
 
-  //부모요소에 추가하기
+  // 글자 잘라내기
+  const maxLength = 4; // 표시할 최대 글자 수
+  const ellipsis = ".."; // 생략 부호
+
+  // targetText 요소의 내용을 가져와서 maxLength를 초과하는 경우 잘라내는 함수
+  function truncateText() {
+    const text = targetText.innerHTML;
+    if (text.length > maxLength) {
+      const truncatedText = text.substring(0, maxLength) + ellipsis;
+      targetText.innerHTML = truncatedText;
+    }
+  }
+
+  // 부모 요소에 추가하기
   document.body.appendChild(target2);
   document.body.appendChild(targetText);
   document.body.appendChild(list_add_btn);
   target2.appendChild(targetText);
   target2.appendChild(list_add_btn);
 
-
   targetText.addEventListener("click", () => {
-    tarText();
-  })
+    truncateText();
+  });
+
+  // 초기에도 문자열 길이 확인 및 자르기 수행
+  truncateText();
+
+  // 잘려서 보이는 p 요소에 아이디 설정
+  const truncatedTextElement = document.createElement("p");
+  truncatedTextElement.id = truncatedTextId;
+
+  // 추가한 p 요소에 대한 참조 유지
+  target2.appendChild(truncatedTextElement);
 });
-function tarText(){
-  //목표 수정하기
-  alert("안녕");
-}
-
-
-
-
